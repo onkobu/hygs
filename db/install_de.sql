c2f1262995bb6af7e3cb96cefd58e48710115397
onkobu <onkobu@onkobutanaake.de>
2019-03-17 20:33:26 +0100

    locale-specific upgrade, app_properties

--- db/install_de.sql
+++ db/install_de.sql
@@ -13,10 +13,13 @@
 .read ddl/004_audit_log_ref2.sql
 .read ddl/005_your_roles.sql
 .read ddl/006_skill_map.sql
+.read ddl/008_skill_views.sql
+.read ddl/010_app_properties.sql
 
 .print DML
 .read dml/cap_capability.sql
 .read dml/cap_category_de.sql
 .read dml/002_prj_role.sql
 .read dml/007_skill_map_de.sql
-
+.read dml/009_capabilities_de.sql
+.read dml/011_app_properties.sql
